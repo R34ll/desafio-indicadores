e2e80cb9cd0c7018b11ca49c3ec3d365b5905f80
r34ll <58672752+R34ll@users.noreply.github.com>
2025-11-19 22:22:14 -0300

    feat(script): Função para atualizar indicadores

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+/*** 
+ * Funções para gerar aleatóriamente o uso de CPU, Memória e Disco
+ * ***/
+function porcentagemAleatoria() {
+    return Math.floor(Math.random() * 100);
+}
+
+function atualizarIndicadores() {
+    document.getElementById('cpu-uso').innerText = porcentagemAleatoria() + "%";
+    document.getElementById('memoria-uso').innerText = porcentagemAleatoria() + " / 16 GB";
+    document.getElementById('disco-uso').innerText = porcentagemAleatoria() + " / 512 GB";
+}
+
+setInterval(atualizarIndicadores, 2000);
+atualizarIndicadores();
+
