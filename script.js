b524c9f72cad6d3354759bfef2b2daabda4cd234
r34ll <58672752+R34ll@users.noreply.github.com>
2025-11-19 22:28:41 -0300

    feat(script): Função para atualizar valores e cores

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 
 
+
 /*** 
  * Funções para gerar aleatóriamente o uso de CPU, Memória e Disco
- * ***/
+ * 
+***/
 function porcentagemAleatoria() {
     return Math.floor(Math.random() * 100);
 }
 
 function atualizarIndicadores() {
-    document.getElementById('cpu-uso').innerText = porcentagemAleatoria() + "%";
-    document.getElementById('memoria-uso').innerText = porcentagemAleatoria() + " / 16 GB";
-    document.getElementById('disco-uso').innerText = porcentagemAleatoria() + " / 512 GB";
+    atualizarIndicador('cpu-uso', porcentagemAleatoria() + "%");
+    atualizarIndicador('memoria-uso', porcentagemAleatoria() + " / 16 GB");
+    atualizarIndicador('disco-uso', porcentagemAleatoria() + " / 512 GB");
 }
+
+function atualizarIndicador(id, valor){
+    document.getElementById(id).innerText = valor;
+    atualizarCorIndicador(id, valor);
+}
+
+
+function atualizarCorIndicador(id, valor) {
+    let elemento = document.getElementById(id);
+    let porcentagem = parseInt(valor);
+
+    if (porcentagem < 50) {
+        elemento.style.color = "green";
+    } else if (porcentagem < 80) {
+        elemento.style.color = "orange";
+    } else {
+        elemento.style.color = "red";
+    }
+}
+
 
 setInterval(atualizarIndicadores, 5000);
 atualizarIndicadores();
